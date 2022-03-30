0da2aa96f8276fc0a6a50299937ad727ae72682e
JuliaJamnicka <julia.jamnicka@gmail.com>
2022-03-30 16:50:04 +0200

    extracted CalculateVolumeCredits method

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -13,15 +13,12 @@ namespace TheatricalPlayersRefactoringKata
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
+            volumeCredits = CalculateVolumeCredits(invoice, plays);
+
             foreach(var perf in invoice.Performances) 
             {
                 var play = plays[perf.PlayID];
                 var thisAmount = GetAmount(play, perf);
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
-
                 // print line for this order
                 result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
@@ -29,6 +26,21 @@ namespace TheatricalPlayersRefactoringKata
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
             return result;
+        }
+
+        private static int CalculateVolumeCredits(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            int volumeCredits = 0;
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayID];
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if ("comedy" == play.Type) volumeCredits += (int) Math.Floor((decimal) perf.Audience / 5);
+            }
+
+            return volumeCredits;
         }
 
         private static int GetAmount(Play play, Performance perf)
